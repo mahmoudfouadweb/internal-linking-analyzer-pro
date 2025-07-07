4b6aa3b42b717fe0b3ae427a5f61af11ba1e2db6
mahmoudfouadweb <mfapolice@gmail.com>
2025-07-07 15:37:24 +0300

    Initial commit: Project structure and setup

---
+++ packages/config/eslint-preset.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+  },
+};
